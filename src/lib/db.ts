a9dcf362c325738356cac104ab63ca152642dde1
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-05-12 15:26:55 +0530

    feat: prisma integrated with PostgreSQL and Neon setup

---
+++ src/lib/db.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '#/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
+
+declare global {
+  var __prisma: PrismaClient | undefined
+}
+
+export const prisma = globalThis.__prisma || new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.__prisma = prisma
+}
