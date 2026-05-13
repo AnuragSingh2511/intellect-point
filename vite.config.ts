ecac778499603f6bc471d43a3166c0ccc06f1dce
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-05-13 13:15:52 +0530

    fix: vite config constructor issue

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,34 @@ import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    nitro({
+      rollupConfig: {
+        external: [
+          '@prisma/client',
+          '@prisma/client/runtime/client',
+          '@prisma/adapter-pg',
+          '@prisma/adapter-libsql',
+          '@libsql/client',
+          'pg',
+        ],
+      },
+      rolldownConfig: {
+        external: [
+          '@prisma/client',
+          '@prisma/client/runtime/client',
+          '@prisma/adapter-pg',
+          '@prisma/adapter-libsql',
+          '@libsql/client',
+          'pg',
+        ],
+      },
+    }),
+    viteReact(),
+  ],
 })
 
 export default config
